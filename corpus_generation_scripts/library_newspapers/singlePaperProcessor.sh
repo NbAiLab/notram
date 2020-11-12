23fe37a7dff9dafa4fcb9c16c567a2a975989f93
Per E Kummervold <per@capia.no>
2020-11-12 15:47:32 +0100

    minor changes to file placement

--- corpus_generation_scripts/library_newspapers/singlePaperProcessor.sh
+++ corpus_generation_scripts/library_newspapers/singlePaperProcessor.sh
@@ -2,5 +2,5 @@
 localfile=`echo $1 |tr -d "'"`
 #echo "${localfile}">>aa.txt
 outputmaster="/disk4/folder1/nancy/content/text/newspaper/nonpdf/text"
-python /home/tensor/notram/toolsForNewspapers/metsParser.py ${localfile} ${outputmaster}
+python /home/tensor/notram/corpus_generation_scripts/library_newspapers/metsParser.py ${localfile} ${outputmaster}
 
