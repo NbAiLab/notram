5b3ae041f5890ad4ec903340db29a53f6096e7ea
Per Egil <per@capia.no>
2021-01-21 14:40:01 +0100

    vocab files

--- vocab_generation/generate_vocab.py
+++ vocab_generation/generate_vocab.py
@@ -1,21 +1,29 @@
 from tokenizers import BertWordPieceTokenizer
+import glob
 
 # Initialize an empty BERT tokenizer
 tokenizer = BertWordPieceTokenizer(
-  clean_text=False,
-  handle_chinese_chars=False,
+  clean_text=True,
+  handle_chinese_chars=True,
   strip_accents=False,
   lowercase=False,
 )
 
-# prepare text files to train vocab on them
-files = ['/var/ml/vocab_generation/sentences_colossal_norwegian_corpus_271120_42.txt']
+
+
+# prepare to train vocab on them
+#files = ['/var/ml/vocab_generation/sentences_colossal_norwegian_corpus_271120_42.txt']
 #files = ['/var/ml/vocab_generation/test100000.txt']
+files = glob.glob('/var/ml/tmp/*.txt')
+
+#print(files)
+#print(len(files))
+#exit()
 
 # train BERT tokenizer
 tokenizer.train(
   files,
-  vocab_size=30000,
+  vocab_size=50000,
   min_frequency=2,
   show_progress=True,
   special_tokens=['[PAD]', '[UNK]', '[CLS]', '[SEP]', '[MASK]'],
@@ -24,4 +32,4 @@ tokenizer.train(
 )
 
 # save the vocab
-tokenizer.save('tokenizer.json')
+tokenizer.save('tokenizer_clean_true_chinese_true.json')
---
+++ vocab_generation/test_vocab.py
@@ -0,0 +1,9 @@
+from tokenizers import BertWordPieceTokenizer, tokenizers
+
+import glob
+
+tokenizer = tokenizers.Tokenizer.from_file('./tokenizer.json')
+
+
+
+
