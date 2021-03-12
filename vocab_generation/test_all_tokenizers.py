6616e5e0be99d2342b662662e62766c29b887604
Per Egil <per@capia.no>
2021-03-12 16:39:03 +0100

    vocab stuff

--- vocab_generation/test_all_tokenizers.py
+++ vocab_generation/test_all_tokenizers.py
@@ -2,13 +2,15 @@ import glob
 from nltk import ngrams
 import random, json, glob, os, codecs, random
 import numpy as np
-import glob
 import ftfy
 
 import argparse
 from os import path
 
 from tokenizers import BertWordPieceTokenizer, tokenizers
+#import bert
+#BertTokenizer = bert.bert_tokenization.FullTokenizer
+#alltokenizer = BertTokenizer("/home/perk/notram/vocab_generation/combined_vocabs/all.txt")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -19,11 +21,13 @@ if __name__ == '__main__':
     singletokens=[]
     totaltokens=[]
     totalwordcount=[]
+    testfiles = sorted(glob.glob('testfiles_1000w/*.txt'))
+    testnames = [x.split(".")[1] for x in testfiles]
 
-    for tok in glob.glob('tokenizers/*.json'):
+    for tok in sorted(glob.glob('tokenizers/*.json')):
 
 
-        for tfile in glob.glob('testfiles_1000w/*.txt'):
+        for tfile in testfiles:
             values = [0] * 1000
             with open(tfile, 'r') as fp:
                 contenttmp = fp.read()
@@ -31,13 +35,15 @@ if __name__ == '__main__':
                 ftfy.fix_text(content)
                 #print("Loading tokenizer: " + str(tok))
                 tokenizer = tokenizers.Tokenizer.from_file(tok)
-
+                #tokenizer = BertTokenizer.from_pretrained("/home/perk/notram/vocab_generation/combined_vocabs/all.txt")
+                
                 tokname=tok.replace(".json","")#tok.split(".")[0].split("/")[1]
                 print("============================================================================")
                 #astr="Freddy ved Nasjonalbiblioteket har laget denne, vi tester Æ Ø Å æ ø å ."
                 astr="Denne gjengen håper at de sammen skal bidra til å gi kvinnefotballen i Kristiansand et lenge etterlengtet løft."
                 #tokenizer._save_pretrained("model/"+tokname)
                 tokenized = tokenizer.encode(str(astr))
+                #peretokens = alltokenizer.tokenize(str(astr))
                 #print(tokenized)
                 #print(type(tokenized))
 
@@ -57,7 +63,11 @@ if __name__ == '__main__':
                 while contenttmp:
                     wordcount += 1
                     tokenized = tokenizer.encode(str(content))
+                    #peretokens = alltokenizer.tokenize(str(content))
+
                     nooftokens = len(tokenized.ids)
+                    #nooftokens = len(peretokens)
+                    
                     sumTokens+= nooftokens
                     values[nooftokens] += 1
                     pstr = str(content)
@@ -73,8 +83,8 @@ if __name__ == '__main__':
             print("                                        ")
 
             print ("Tokenizer" + str(tok))
-            print("The file " + tfile + " contains " + str(wordcount) + " words and " + str(sumTokens) + " tokens")
-            print(str(values[1]) + " of " + str(wordcount) + " words in file are single tokens, percentage: " + str(values[1]/wordcount))
+            print("The file " + tfile + " contains 1000  words and " + str(sumTokens) + " tokens")
+            print(str(values[1]) + " of 1000 words in file are single tokens, percentage: " + str(values[1]/wordcount))
             print("============================================================================")
             if tokname not in toknames:
                 toknames.append(tokname)
@@ -100,20 +110,17 @@ if __name__ == '__main__':
     cnt_tok=0
     cnt=0
     print( "---------------------------------------------------------------------------------------------------------------------------------")
-    print("Tokenizer" + "\t\t\t\t"+ "Juss" + "\t\t\t"+ "avis" + "\t\t\t"+ "Rapport"  + "\t\t\t"+ "Roman" )
+    print("Tokenizer" + " "+ testnames[0] + " "+ testnames[1] + " "+ testnames[2] )
     print("---------------------------------------------------------------------------------------------------------------------------------")
-    while (cnt_tok < len(toknames)):
-        tname=""
+   
+    for tname in toknames:
         tokspacelen=int(len(toknames[cnt_tok])/8)
-        tname = toknames[cnt_tok]
         j=0
         #print(numberoftabs)
         while tokspacelen<=numberoftabs:
             tname+="\t"
             tokspacelen+=1
 
-        print(tname +  str(totaltokens[cnt])+ "("+ str(singletokens[cnt])+")" + "\t\t" + str(totaltokens[cnt+1])+ "("+ str(singletokens[cnt+1])+")" + "\t\t" + str(totaltokens[cnt+2])+ "("+ str(singletokens[cnt+2])+")" \
-              + "\t\t" + str(totaltokens[cnt+3])+ "("+ str(singletokens[cnt+3])+")" + "\t\t" )
-        cnt_tok += 1
-        cnt+=4
+        print(tname + " " + str(totaltokens[cnt]) + " " + str(totaltokens[cnt+1])+ " " + str(totaltokens[cnt+2])+ " " )
+        cnt+=3
     print("---------------------------------------------------------------------------------------------------------------------------------")
