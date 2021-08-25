db5ceaaa2ba0d5d968c264734e87b6b63a74f4a5
Per E Kummervold <per@capia.no>
2021-08-25 14:19:20 +0200

    Cleaned up the sub-corpora file

--- corpus_generation_scripts/create_subcorpus_sami.py
+++ corpus_generation_scripts/create_subcorpus_sami.py
@@ -5,7 +5,6 @@
 from tqdm import tqdm
 from utils.misc import ArgParseDefault, add_bool_arg
 import jsonlines, json
-import logging
 
 def main(args):
     with open(args.json_file_list) as file:
@@ -16,42 +15,33 @@ def main(args):
         docid_list = file.readlines()
         docid_list = [line.rstrip() for line in docid_list]
 
+    tot = 0    
+    hits = 0
     for myfile in tqdm(json_file_list): 
         with jsonlines.open(myfile) as reader:
             with jsonlines.open(args.output_file, mode='w') as writer:
                 for n,obj in enumerate(reader):
+                    tot += 1
                     if obj['id'] in docid_list:
+                        hits += 1
+                        # Prints the json-line to file
                         writer.write(obj)
+                        # Prints the docid to screen
                         print(obj['id'])
 
     
-    #Save is as jsonl
-    #output_filename = os.path.join(args.output_folder, os.path.basename(args.input_file))
-    #save_jsonl(data, output_filename)
-    #logger.info(f'*** Finished processing file. Result has {len(data)} posts. Result is written to {os.path.join(args.output_folder, os.path.basename(args.input_file))}')
-    #print(f'*** Finished processing file. Result has {len(data)} posts. Result is written to {os.path.join(args.output_folder, os.path.basename(args.input_file))}')
+    print(f'*** Finished processing {len(json_file_list} json-files. Result has {hits} documents of a total {tot} documents evaluated. Result is written to {args.output_file}.')
 
 def parse_args():
     parser = ArgParseDefault()
     parser.add_argument('--json_file_list', required=True, help='List of json-files that should be processed')
     parser.add_argument('--output_file', required=True, help='Path to output file. Will replace existing file.')
     parser.add_argument('--docid_list',required=True, default="config.json", help='List of docids that should be included')
-    parser.add_argument('--log_level',required=False, default="INFO", help='Set logging level to DEBUG to get a report on all decisions')
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = parse_args()
-   
-    # Invoke logger globally
-    logger= logging.getLogger()
-    if args.log_level == "INFO":
-       logger.setLevel(logging.INFO)
-    elif args.log_level =="DEBUG":
-       logger.setLevel(logging.DEBUG)
-    else:
-        print("Log level not accepted")
-        exit()
     main(args)
 
 
