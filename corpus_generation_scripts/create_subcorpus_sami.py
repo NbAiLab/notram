7374692f9caa0ff93b78dd73eaaaadc117eb76b4
Per E Kummervold <per@capia.no>
2021-08-25 14:40:29 +0200

    Add verbosity to subcorpus script

--- corpus_generation_scripts/create_subcorpus_sami.py
+++ corpus_generation_scripts/create_subcorpus_sami.py
@@ -17,20 +17,30 @@ def main(args):
 
     tot = 0    
     hits = 0
-    for myfile in tqdm(json_file_list): 
-        with jsonlines.open(myfile) as reader:
-            with jsonlines.open(args.output_file, mode='w') as writer:
+    docid_notfound_list = docid_list.copy()
+
+    with jsonlines.open(args.output_file, mode='w') as writer:
+        for myfile in tqdm(json_file_list): 
+            with jsonlines.open(myfile) as reader:
                 for n,obj in enumerate(reader):
                     tot += 1
                     if obj['id'] in docid_list:
-                        hits += 1
-                        # Prints the json-line to file
+                        hits += 1                        
+                        try:
+                            docid_notfound_list.remove(obj['id'])
+                        except:
+                            print("Seems like there are multiple versions with this docid in the corpus. This should not happen.")
+
+                        #Prints the json-line to file
                         writer.write(obj)
+                        
                         # Prints the docid to screen
                         print(obj['id'])
 
     
-    print(f'*** Finished processing {len(json_file_list} json-files. Result has {hits} documents of a total {tot} documents evaluated. Result is written to {args.output_file}.')
+    print(f'*** Finished processing {len(json_file_list)} json-files. Result has {hits} documents of a total {tot} documents evaluated. Result is written to {args.output_file}.')
+    print(f'*** A total of {len(docid_notfound_list)} was not found. Listing here for reference:')
+    print(docid_notfound_list)
 
 def parse_args():
     parser = ArgParseDefault()
