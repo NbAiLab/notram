6cc94c84a8f3ba832ae3804704c80c20aed09b5e
Per E Kummervold <per@capia.no>
2021-09-07 16:01:20 +0200

    minor change

--- corpus_generation_scripts/utils/generating_test_corpus.py
+++ corpus_generation_scripts/utils/generating_test_corpus.py
@@ -1,6 +1,7 @@
 ## Generates sharded corpus test files 
 ## When done just run 'gzip *.json'
 
+num_lines = 10000
 num_shards = 5
 final_shard = f"{num_shards:04d}"
 
@@ -9,7 +10,7 @@ for shard in range(1,num_shards+1):
     corpus_file_name = "test_corpus_train-shard-"+this_shard+"-of-"+final_shard+".json"
 
     with open(corpus_file_name,'w') as f:
-        for n in range(1,10001):
+        for n in range(1,num_lines+1):
             jsonline = "{\"id\": \"doc_"+str(this_shard)+"_"+str(n)+"\", \"text\": \"This is line #"+str(n)+" of shard "+this_shard+" of "+final_shard+". Testing 123 æøåÆØÅ.\"}\n"
             f.write(jsonline)
 
