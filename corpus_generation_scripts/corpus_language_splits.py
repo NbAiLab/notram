53186600526a5c64bfbe2d309781ee9d565edf34
FreddyWetjen <freddy.wetjen@gmail.com>
2021-11-06 11:01:48 +0100

    Add files via upload

---
+++ corpus_generation_scripts/corpus_language_splits.py
@@ -0,0 +1,117 @@
+import sys
+import os
+import math
+import numpy as np
+import glob
+import argparse
+import math
+import subprocess
+
+from os import path
+import unicodedata
+from string import printable
+import ftfy
+import jsonlines
+import json
+import pickle
+import fasttext
+import hashlib
+import sys
+import shutil
+from datetime import date,datetime
+
+from tqdm import tqdm
+import csv
+from urllib.parse import urlparse
+import collections
+import logging
+import gzip
+
+def fileexists(absfile):
+    if os.path.isfile(absfile):
+        return True
+    else:
+        return False
+
+
+def directoryexists(dir):
+    if os.path.isdir(dir):
+        return True
+    else:
+        return False
+
+def is_json(myjson):
+  try:
+    json.loads(myjson)
+  except ValueError as e:
+    return False
+  return True
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--corpus_input_file', help='Input file to be language split', required=True, type=str)
+    parser.add_argument('--corpus_output_dir', help='Master output dir for language splitted files', required=True, type=str)
+    parser.add_argument('--languages_separated', help='languages to be separated, default="no,nn,en,sv,da,is,fo"', required=False,
+                        type=str,default='no,nn,en,sv,da,is,fo')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    outdir=""
+    lastchar = str(args.corpus_output_dir)[-1]
+    if lastchar == "/":
+        outdir=str(args.corpus_output_dir)[:-1]
+    else:
+        outdir = str(args.corpus_output_dir)
+
+
+
+    print("start creating language split for: " + args.corpus_input_file)
+    print("Output from split process stored in:  " + args.corpus_output_dir + "/language_splits")
+    masteroutputdir=outdir.strip() + "/language_splits"
+    relativefilename=args.corpus_input_file.split("/")[-1]
+    subfolders = args.languages_separated.split(",")
+    os.makedirs(masteroutputdir, exist_ok=True)
+    indexdict = {}
+
+
+    for fdir in subfolders:
+        outputdir=masteroutputdir +"/" + fdir +"/"
+        if directoryexists(outputdir) == False:
+            os.makedirs(outputdir)
+        indexdict[fdir] = open(outputdir+ relativefilename,"w")
+
+    if  directoryexists(masteroutputdir + "/other") == False:
+            os.makedirs(masteroutputdir + "/other")
+    indexdict['other'] = open(masteroutputdir + "/other/" + relativefilename,"w")
+
+
+    f=args.corpus_input_file
+    print("processing file: " +f)
+    cnt=0
+    with open(f) as infile:
+        for line in infile:
+            #print("processing line: \n\n" + line + "\n\n")
+            #print(masteroutputdir)
+            cnt += 1
+            if (cnt % 1000000000) == 0:
+                print(".", end="", flush=True)
+
+            if is_json(line) == False:
+                continue
+
+            j = json.loads(line)
+            lang =j['lang_fasttext']
+            if lang in args.languages_separated:
+                indexdict[lang].write(line)
+            else:
+                indexdict['other'].write(line)
+    print()
+    for myfile in indexdict.values():
+        myfile.write("\n")
+        myfile.close()
+
+
